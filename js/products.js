0c66f0d1e025d1c2926fe29d239dbfd0e0d3f8d4
Dracoptera <dracoptera@gmail.com>
2019-08-23 01:57:44 -0300

    Remover función sort
    Remover código de función sort del listado de productos.
    Remover páginas de login de testeo.
    Añadir PDF de abstracción obligatorio.

--- js/products.js
+++ js/products.js
@@ -1,41 +1,10 @@
-const ORDER_ASC_BY_NAME = "AZ";
-const ORDER_DESC_BY_NAME = "ZA";
-const ORDER_BY_PROD_COUNT = "Cant.";
-
-
 var currentProductsArray = [];
-var currentSortCriteria = undefined;
 var minCount = undefined;
 var maxCount = undefined;
 
 
 function sortProducts(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_NAME)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.name < b.name ){ return -1; }
-            if ( a.name > b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC_BY_NAME){
-        result = array.sort(function(a, b) {
-            if ( a.name > b.name ){ return -1; }
-            if ( a.name < b.name ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_BY_PROD_COUNT){
-        result = array.sort(function(a, b) {
-            let aCount = parseInt(a.productCount);
-            let bCount = parseInt(b.productCount);
 
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
-            return 0;
-        });
-    }
-
-    return result;
 }
 
 function showProductsList(array){

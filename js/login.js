409c387ecf8896b4eefcb7032e5d6d3416e15516
Alexa <52338361+Dracoptera@users.noreply.github.com>
2019-09-24 12:46:03 -0300

    Limpiar código

--- js/login.js
+++ js/login.js
@@ -94,4 +94,13 @@ function validate(form) {
     //     }
     //     } else {  alert("Usuario inválido")
     //     }
+
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      }
+
 localStorage.clear()

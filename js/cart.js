6a2bdf16803bf3bf87d035d57afe4084c07bc62c
Dracoptera <dracoptera@gmail.com>
2019-10-19 21:02:50 -0300

    Añadir carrito
    Añadir carrito con función para actualizar el precio subtotal.

--- js/cart.js
+++ js/cart.js
@@ -8,13 +8,25 @@ const CREDIT_CARD_PAYMENT = "Tarjeta de crédito";
 const BANKING_PAYMENT = "Transferencia bancaria";
 let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
+let MONEY_SYMBOL = "$";
+let DOLLAR_CURRENCY = "Dólares (USD)";
+let PESO_CURRENCY = "Pesos Uruguayos (UYU)";
+let DOLLAR_SYMBOL = "USD ";
+let PESO_SYMBOL = "UYU ";
+
 //Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts(){
 
 }
 
-function updateSubtotal(){
+function updateSubtotal(cost){
 
+    let subtotalHTML = document.querySelector("#currentSubtotal")
+    let count = document.querySelector("#currentCount").value
+    let newSubtotal = count * cost 
+
+    subtotalHTML.innerHTML = MONEY_SYMBOL + ` ` + newSubtotal
+    
 }
 
 function showPaymentTypeNotSelected(){
@@ -25,7 +37,34 @@ function hidePaymentTypeNotSelected(){
 
 }
 
-function showArticles(articles){
+function showArticles(article){
+
+    let tableContent = ""
+
+    for(let i = 0; i < article.length; i++){
+
+        let currentArticle = article[i];
+
+        tableContent += `
+        <tr>
+        <td><img src="`+ currentArticle.src + `" width="100" height="100">`+ currentArticle.name + `</td>
+        <td><div class="form-group w-25"><input type="number" value="`+ currentArticle.count + `" min="0" class="form-control" id="currentCount"></div></td>
+        <td>`+ currentArticle.currency + ` `+ currentArticle.unitCost + `</td>
+        <th id="currentSubtotal"> - </th>
+        </tr>
+        `
+        document.querySelector("#articleContainer").innerHTML = tableContent
+
+        let currentCount = document.querySelector("#currentCount")
+    
+        let articleCost = currentArticle.unitCost
+    
+        updateSubtotal(articleCost)
+
+        currentCount.addEventListener("change", function(){
+            updateSubtotal(articleCost)
+        })
+    }
 
 }
 
@@ -33,5 +72,18 @@ function showArticles(articles){
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(CART_INFO_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+             cart = resultObj.data;
+
+            // Añadir items al carrito
+            showArticles(cart.articles)
+
+
+        }
+        
+
+    });
 
 });
--- js/login.js
+++ js/login.js
@@ -94,4 +94,18 @@ function validate(form) {
     //     alert("Contraseña inválida")
     //     }
     //     } else {  alert("Usuario inválido")
+<<<<<<< Updated upstream
     //     }
+=======
+    //     }
+
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+      }
+
+localStorage.clear();
+>>>>>>> Stashed changes
